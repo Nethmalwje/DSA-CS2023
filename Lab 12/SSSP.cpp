75dbc306e39cfaa27a822aadc7b7f1acdf6733ea
Nethmal Wijesinghe <130041936+Nethmalwje@users.noreply.github.com>
2023-06-01 16:16:14 +0530

    Update SSSP.cpp

--- Lab 12/SSSP.cpp
+++ Lab 12/SSSP.cpp
@@ -6,57 +6,57 @@
 using namespace std;
 
 // Function to find the vertex with minimum distance value,
-int minDistance(int dist[], bool sptSet[])
+int minDistance(int time[], bool VertexVisited[])
 {
     
     int min = INT_MAX, min_index;
 
     // Find the vertex with minimum distance
     for (int v = 0; v < V; v++)
-        if (sptSet[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
+        if (VertexVisited[v] == false && time[v] <= min)
+            min = time[v], min_index = v;
 
     return min_index;
 }
 
 // Function for print the constructed distance array
-void printSolution(int dist[], int n)
+void printSolution(int time[], int n)
 {
     cout<<"Vertex   Distance from Source\n";
     for (int i = 0; i < V; i++)
-        cout<<i<<"-------"<< dist[i]<<endl;
+        cout<<i<<"-------"<< time[i]<<endl;
         }
 // Dijkstra's single source shortest path algorithm
 void dijkstra(int graph[V][V], int src)
 {
-    int dist[V]; // array used to store the shortest distance from source 
+    int time[V]; // array used to store the shortest distance from source 
 
-    bool sptSet[V]; 
+    bool VertexVisited[V]; 
 
-    // Initialize all distances as INFINITE and sptSet[] as false
+    // Initialize all distances as INFINITE and VertexVisited[] as false
     for (int i = 0; i < V; i++)
-        dist[i] = INT_MAX, sptSet[i] = false;
+        time[i] = INT_MAX, VertexVisited[i] = false;
 
     // Distance of source vertex from itself is always 0
-    dist[src] = 0;
+    time[src] = 0;
 
     // Find shortest path for all vertices
     for (int count = 0; count < V - 1; count++) {
         // Pick minimum distance vertex from the set of vertices not yet processed.
-        int u = minDistance(dist, sptSet);
+        int u = minDistance(time, VertexVisited);
 
         // Mark the  vertex as processed
-        sptSet[u] = true;
+        VertexVisited[u] = true;
 
-        // Update dist[v] only if it is not in sptSet, there is an edge from u to v,and the total weight of path from src to v through u is smaller than current value of dist[v]
+        // Update time[v] only if it is not in VertexVisited, there is an edge from u to v,and the total weight of path from src to v through u is smaller than current value of time[v]
         for (int v = 0; v < V; v++)
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-                && dist[u] + graph[u][v] < dist[v])
-                dist[v] = dist[u] + graph[u][v];
+            if (!VertexVisited[v] && graph[u][v] && time[u] != INT_MAX
+                && time[u] + graph[u][v] < time[v])
+                time[v] = time[u] + graph[u][v];
     }
 
     // Print the constructed distance array
-    printSolution(dist, V);
+    printSolution(time, V);
 }
 
 int main()
